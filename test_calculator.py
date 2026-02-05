7c5643238309bc82bf18154576e6e70013dbd68f
GUDDATI DHARSHINI <133137635+Dharshini310@users.noreply.github.com>
2026-02-05 14:34:49 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
-from calculator import Test
+from calculator import add,substract
 
 def test_add():
     assert add(2,3) == 5
 
 def test_sub():
-    assert sub(5,2) == 3
+    assert substract(5,2) == 3
