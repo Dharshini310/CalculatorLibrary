5b8502c1bf615433d6ab3bc00bde874c28d1b58f
GUDDATI DHARSHINI <133137635+Dharshini310@users.noreply.github.com>
2026-02-05 14:51:36 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -5,3 +5,9 @@ def test_add():
 
 def test_sub():
     assert substract(5,2) == 3
+def test_mul():
+    assert mul(2,3) == 6
+def test_div():
+    assert div(6,2) == 3
+def test_mod():
+    assert mod(3%4) == 3
